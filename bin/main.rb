5e612e21b4f17a96b148190b4d57ecdca18d9e45
lamiasristy <lamiasristy@gmail.com>
2020-09-19 20:25:37 +0200

    Built with chnags in main.rb and scraper.rb files.

--- bin/main.rb
+++ bin/main.rb
@@ -3,13 +3,10 @@
 require_relative '../lib/parser.rb'
 require_relative '../lib/scraper.rb'
 
-recipe_spec = {
-    base_url: 'https://natashaskitchen.com/category/',
-    type: 'breakfast',
-    prep_time_min: 5
-}
-recipes = get_data(recipe_spec)
+
+crawler = RecipeCrawler.new('breakfast', 5)
+recipes = crawler.get_data()
 puts "Count: #{recipes.count}"
 recipes.each do |item|
-    puts "Recipe Name = #{item[:name]} prep_time=#{item[:prep_time_min]}"
+  puts "Recipe Name = #{item[:name]} prep_time=#{item[:prep_time_min]}"
 end
--- lib/parser.rb
+++ lib/parser.rb
@@ -3,10 +3,15 @@ require 'httparty'
 require 'nokogiri'
 
 class Parser 
-    def parse_page(url)
+    def self.parse_page(url)
         unparsed_page = HTTParty.get(url)
         parsed_page = Nokogiri::HTML.parse(unparsed_page.body)
         parsed_page
+    end
+
+    def self.parse_url(element)
+        url = element.css('a').attribute('href').value
+        url
     end
 end
 
