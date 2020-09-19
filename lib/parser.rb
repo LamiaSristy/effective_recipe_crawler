8ebe90dcee7f0ea95fd7e0d3d7f252ac2293d860
lamiasristy <lamiasristy@gmail.com>
2020-09-19 20:30:54 +0200

    Built with changes in scraper.rb file and added linters files

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,15 +4,13 @@ require 'nokogiri'
 
 class Parser 
     def self.parse_page(url)
-        unparsed_page = HTTParty.get(url)
-        parsed_page = Nokogiri::HTML.parse(unparsed_page.body)
-        parsed_page
+      unparsed_page = HTTParty.get(url)
+      parsed_page = Nokogiri::HTML.parse(unparsed_page.body)
+      parsed_page
     end
 
     def self.parse_url(element)
-        url = element.css('a').attribute('href').value
-        url
+      url = element.css('a').attribute('href').value
+      url
     end
 end
-
-
