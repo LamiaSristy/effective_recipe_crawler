23825e76288d51ae91359c3e1c488d2624909c4d
lamiasristy <lamiasristy@gmail.com>
2020-09-19 20:46:51 +0200

    Built with changes in scarper_spec.rb and parser_spec.rb files.

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,1 +1,17 @@
 require './lib/scraper.rb'
+
+RSpec.describe 'scraper class methods' do
+
+    context 'test list_scraper' do
+
+    end
+
+    context 'test  scraper' do
+
+    end
+
+    context 'test get_data' do
+
+    end
+
+end
