03172faf2157297671dbc017ec21a3a9f306ccc3
lamiasristy <lamiasristy@gmail.com>
2020-09-20 00:17:22 +0200

    Built with tast cases for parse_spec.rb files.

--- bin/main.rb
+++ bin/main.rb
@@ -8,5 +8,5 @@ crawler = RecipeCrawler.new('breakfast', 5)
 recipes = crawler.get_data()
 puts "Count: #{recipes.count}"
 recipes.each do |item|
-  puts "Recipe Name = #{item[:name]} prep_time=#{item[:prep_time_min]}"
+  puts "Recipe Name = #{item[:name]}, prep_time=#{item[:prep_time_min]}"
 end
