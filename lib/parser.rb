ad1da30dc08e4a429383c01bca905443331bcb95
lamiasristy <lamiasristy@gmail.com>
2020-09-20 13:29:10 +0200

    Built with changes in Testing files and fixed the linters errors.

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,15 +2,15 @@ require 'byebug'
 require 'httparty'
 require 'nokogiri'
 
-class Parser 
-    def self.parse_page(url)
-      unparsed_page = HTTParty.get(url)
-      parsed_page = Nokogiri::HTML.fragment(unparsed_page.body)
-      parsed_page
-    end
+class Parser
+  def self.parse_page(url)
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML.fragment(unparsed_page.body)
+    parsed_page
+  end
 
-    def self.parse_url(element)
-      url = element.css('a').attribute('href').value
-      url
-    end
+  def self.parse_url(element)
+    url = element.css('a').attribute('href').value
+    url
+  end
 end
